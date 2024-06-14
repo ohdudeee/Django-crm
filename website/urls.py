cbe2269d271177bfaf87f2e9b2d82832fcbd98bd
ohdudeee <romeshmeshram15@gmail.com>
2024-06-14 23:34:34 +0530

    adding info card

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path('login_user/',views.login_user,name='login'),
     path('logout_user/',views.logout_user,name='logout'),
     path('register/',views.register_user,name='register'),
+    path('record/<int:pk>',views.customer_record,name='record'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .form import SignUpForm
+from .models import Record
+
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+    
     # if user logging in
     if request.method =='POST':
         username=request.POST['username']
@@ -17,7 +21,7 @@ def home(request):
             messages.success(request,"There is some error..try again....")
             return redirect('home')
     else:
-       return render(request,'home.html',{})
+       return render(request,'home.html',{'records': records})
 
 
 def logout_user(request):
@@ -37,5 +41,15 @@ def register_user(request):
             messages.success(request,'you have registered')
             return redirect('home')
     else:
-     form=SignUpForm()
-     return render(request, "register.html",{'form':form})
+       form=SignUpForm()
+       return render(request, 'register.html',{'form':form})
+    
+    return render(request, 'register.html',{'form':form})
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"you must log in to view this...")
+        return redirect('home')
